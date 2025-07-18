817953020f4cc2ad534f77de5e03ea651eec15a1
Loreti <111466632+PedroLoreti@users.noreply.github.com>
2025-07-17 21:03:29 -0300

    fix: foi consertado a pagina de separacao do jirau

--- frontend/src/components/SeparacaoJirau/SeparacaoJirauMain/SeparacaoJirauMain.jsx
+++ frontend/src/components/SeparacaoJirau/SeparacaoJirauMain/SeparacaoJirauMain.jsx
@@ -4,8 +4,14 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import styles from "./styles.module.scss";
 
 const separacaoSchema = z.object({
-  cracha: z.number().min(6, "O campo crachá invalido"),
-  pedido: z.number().min(7, "O campo pedido invalido"),
+  cracha: z
+    .string()
+    .length(6, "O campo crachá precisa ter 6 caracteres")
+    .regex(/^\d+$/, "O campo crachá deve conter apenas números"),
+  pedido: z
+    .string()
+    .length(7, "O campo pedido precisa ter 6 caracteres")
+    .regex(/^\d+$/, "O campo pedido deve conter apenas números"),
 });
 
 
@@ -13,8 +19,9 @@ export const SeparacaoJirauMain = () => {
   const { register, handleSubmit, formState: { errors } } = useForm({
     resolver: zodResolver(separacaoSchema),
   });
-
+  
   const onSubmit = (data) => {
+
     console.log(data);
   };
 
@@ -23,10 +30,10 @@ export const SeparacaoJirauMain = () => {
       <h1 className="tittle3">Separação Jirau</h1>
       <p>Realize a separação dos pedidos utilizando o crachá e o número do pedido.</p>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input placeholder="Bipe o crachá" {...register("cracha", { valueAsNumber: true })} />
+        <input placeholder="Bipe o crachá" {...register("cracha")} />
         {errors.cracha && <p>{errors.cracha.message}</p>}
     
-        <input placeholder="Bipe o Pedido" {...register("pedido", { valueAsNumber: true })} />
+        <input placeholder="Bipe o Pedido" {...register("pedido")} />
         {errors.pedido && <p>{errors.pedido.message}</p>}
 
         <button type="submit">Enviar</button>
